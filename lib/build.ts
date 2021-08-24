3d22aa046f397531f46bcef2a690abcd1aaa092b
ZYROUGE <zyrouge@hotmail.com>
2021-08-24 18:27:54 +0530

    fix: public not being copied on build

--- lib/build.ts
+++ lib/build.ts
@@ -1,8 +1,9 @@
-import fs from "fs-extra";
-import { dirname, join, extname } from "path";
+import { copyFile, ensureDir, writeFile, rm } from "fs-extra";
+import { dirname, extname } from "path";
 import { Config } from "./config";
 import { Renderer } from "./renderer";
 import { TexaEventEmitter, TexaEventSkeleton } from "./helpers/eventer";
+import { filesIn } from "./helpers/filesIn";
 
 export interface TexaBuilderEvents extends TexaEventSkeleton {
     folder: (path: string) => void;
@@ -18,54 +19,46 @@ export class TexaBuilder extends TexaEventEmitter<TexaBuilderEvents> {
         this.config = config;
     }
 
-    async file(path: string): Promise<void> {
-        this.dispatch("file", path);
-
+    async _build(path: string): Promise<void> {
         const ext = extname(path);
         const out = path.replace(this.config.contents, this.config.outputDir);
 
-        await fs.ensureDir(dirname(out));
+        await ensureDir(dirname(out));
         switch (ext) {
             case ".html": {
                 const rendered = await Renderer.html(path, this.config, {});
-                await fs.writeFile(out, rendered);
+                await writeFile(out, rendered);
                 break;
             }
 
             case ".md": {
                 const rendered = await Renderer.md(path, this.config, {});
-                await fs.writeFile(out.replace(/\.md/, ".html"), rendered);
+                await writeFile(out.replace(/\.md/, ".html"), rendered);
                 break;
             }
 
             default:
-                await fs.copyFile(path, out);
+                await copyFile(path, out);
                 break;
         }
     }
 
-    async folder(path: string): Promise<void> {
-        this.dispatch("folder", path);
-
-        for (const name of await fs.readdir(path)) {
-            const p = join(path, name);
-            const lstat = await fs.lstat(p);
-
-            if (lstat.isFile()) {
-                await this.file(p);
-            } else {
-                await this.folder(p);
-            }
-        }
-    }
-
     async start(): Promise<void> {
-        await fs.rm(this.config.outputDir, {
+        await rm(this.config.outputDir, {
             recursive: true,
             force: true,
         });
 
-        await this.folder(this.config.contents);
-        await this.folder(this.config.public);
+        for (const file of await filesIn(this.config.contents)) {
+            this.dispatch("file", file);
+            await this._build(file);
+        }
+
+        for (const file of await filesIn(this.config.public)) {
+            this.dispatch("file", file);
+            const out = file.replace(this.config.public, this.config.outputDir);
+            await ensureDir(dirname(out));
+            await copyFile(file, out);
+        }
     }
 }
